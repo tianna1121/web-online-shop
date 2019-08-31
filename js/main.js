b0a579734e05d0237587b3809d85025734c230d6
yang zhang <tianna1121@gmail.com>
2019-08-31 20:44:47 +0800

    Add the second sliders

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ $(".slider-one")
     prevArrow: ".site-slider .slider-btn .prev",
     nextArrow: ".site-slider .slider-btn .next"
   });
+
+/* Second Slide */
+$(".slider-two")
+  .not(".slick-initialized")
+  .slick({
+    prevArrow: ".site-slider-two .prev",
+    nextArrow: ".site-slider-two .next",
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000
+  });
