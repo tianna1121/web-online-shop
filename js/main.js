bcc10adf66d4231a069bae834eb1b353932c8b7c
yang zhang <tianna1121@gmail.com>
2019-08-31 20:18:15 +0800

    Add some sliders

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* First Slide */
+$(".slider-one")
+  .not(".slick-initialized")
+  .slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    prevArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next"
+  });
